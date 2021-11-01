7ed37b998d0a955f4e5d62ef964a1b56fead375b
nopynospy <nopynospy@gmail.com>
2021-11-01 18:03:40 +0800

    Init

---
+++ test_db.py
@@ -0,0 +1,30 @@
+import os.path
+from dotenv import load_dotenv
+import pymongo
+
+load_dotenv()
+endpoint = os.getenv('HEADER') +  os.getenv('DEV_ID') + ":" +os.getenv('DEV_PW') +  os.getenv('ENDPOINT')
+
+client = pymongo.MongoClient(endpoint)
+db = client["steamboat-squad"]
+
+mycol = db["noun"]
+
+myquery = { "word": "cat" }
+
+mydoc = mycol.find(myquery)
+
+for x in mydoc:
+  print(x)
+
+# mylist = [
+#   { "word": "for"},
+#   { "word": "and"},
+#   { "word": "nor"},
+#   { "word": "but"},
+#   { "word": "or"},
+#   { "word": "yet"},
+#   { "word": "so"},
+# ]
+
+# x = mycol.insert_many(mylist)
