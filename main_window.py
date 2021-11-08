76dc9a5f74569d9c5d83733ee540bd89dd378fe2
nopynospy <nopynospy@gmail.com>
2021-11-08 21:26:09 +0800

    Update UI: add subwindow. Init foodbase xml

--- main_window.py
+++ main_window.py
@@ -8,18 +8,30 @@ class MainWindow(QWidget):
     def __init__(self):
         # Initialize UI
         super(MainWindow, self).__init__()
-        self.userInputText = ""
+        self.suggestions = [
+            {"word": "cat", "distance": 5},
+            {"word": "fish", "distance": 3},
+            {"word": "carrot", "distance": 8},
+            {"word": "egg", "distance": 9},
+            {"word": "onion", "distance": 12},
+            {"word": "seaweed", "distance": 4},
+            {"word": "apple", "distance": 9},
+            {"word": "car", "distance": 15},
+            {"word": "house", "distance": 11},
+            {"word": "old", "distance": 7},
+            {"word": "new", "distance": 7},
+            {"word": "past", "distance": 13},
+        ]
         self.initUI()
 
     def initUI(self):
-        # Split the window into 2 main frames
-        self.hbox = QHBoxLayout()
+        # create 2 windows: main for text input, sub window for text suggestion
+        self.mainVBox = QVBoxLayout()
         self.inputFrame = QFrame()
         self.inputFrame.setFrameShape(QFrame.StyledPanel)
-        self.suggestFrame = QFrame()
-        self.suggestFrame.setFrameShape(QFrame.StyledPanel)
+        self.suggestPopUp = SuggestWindow()
 
-        # The first frame on top will be text area for user input. Add title.
+        # The main window will have text area for user input. Add title and layout.
         self.inputLayout = QVBoxLayout()
         self.inputLayout.addWidget(QLabel("<h1>Welcome to test</h1>"))
 
@@ -37,6 +49,9 @@ class MainWindow(QWidget):
         # Button: Save as Word document
         self.saveWordBtn = QPushButton()
         self.saveWordBtn.setText("Save as Word file")
+        self.saveWordBtn.clicked.connect(
+            lambda checked: self.toggleWindow(self.suggestPopUp)
+        )
         self.inputOptionsLayout.addWidget(self.saveWordBtn)
 
         # Button: clear text area
@@ -60,20 +75,17 @@ class MainWindow(QWidget):
         self.editCursor = QTextCursor(self.textedit.textCursor())
         self.inputLayout.addWidget(self.textedit)
 
-        # Set layouts of both first main frame and the frame in the first main frame
+        # Set layouts of both first main frame and the frame in the main window
         self.inputOptions.setLayout(self.inputOptionsLayout)
         self.inputFrame.setLayout(self.inputLayout)
 
-        # Add a splitter to divide both main frames to allow user to drag and expand frame size
-        self.frameSplitter = QSplitter(Qt.Vertical)
-        self.frameSplitter.addWidget(self.inputFrame)
-        self.frameSplitter.addWidget(self.suggestFrame)
-        self.hbox.addWidget(self.frameSplitter)
-        self.setLayout(self.hbox)
+        # Add input frame to main window
+        self.mainVBox.addWidget(self.inputFrame)
+        self.setLayout(self.mainVBox)
         QApplication.setStyle(QStyleFactory.create('Cleanlooks'))
         
         # Set main window layout and title
-        self.setGeometry(500, 500, 500, 400)
+        self.setGeometry(100, 100, 500, 600)
         self.setWindowTitle('Test')
         self.show()
 
@@ -96,10 +108,17 @@ class MainWindow(QWidget):
         print(self.textedit.toPlainText())
         self.updateWordCount()
     
-    # Function to update word count
+    # Function to update word count and limit word number to 500
     def updateWordCount(self):
-        newCount = str(len(self.textedit.toPlainText().split())) + "/500words"
-        self.wordCountLabel.setText(newCount)
+        text = self.textedit.toPlainText()
+        newCount = len(text.split())
+        countText = str(newCount) + "/500words"
+        self.wordCountLabel.setText(countText)
+        maxInputLen = 500
+        if newCount > maxInputLen:
+            text = ' '.join(text.split()[:maxInputLen])
+            self.textedit.setPlainText(text)
+            self.getEditCursor().setPosition(len(text))
 
     # Function to get text edit cursor
     def getEditCursor(self):
@@ -111,12 +130,48 @@ class MainWindow(QWidget):
             index = self.getEditCursor().position() - 1
             current = self.textedit.toPlainText()
             print(current[index])
+            if current[index] == " " or current[index] == "\n":
+                print("space or blank, suggest new word")
+            else:
+                print("cursor in word, use spell check")
+
+    # # Function to dynamically generate suggestion buttons
+    # def generateButtons(self):
+    #     for s in self.suggestions:
+    #         newBtn = QPushButton()
+    #         newBtn.setText(s["word"])
+    #         count = self.suggestOptionsLayout.count() - 1
+    #         self.suggestOptionsLayout.insertWidget(count, newBtn)
+    #         # self.suggestOptions.setLayout(self.suggestOptionsLayout)
+
+    # Show sub window, such as spelling suggestion window
+    def toggleWindow(self, window):
+        if window.isVisible():
+            window.hide()
+
+        else:
+            window.show()
+
+class SuggestWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.hbox = QHBoxLayout()
+        self.scrollArea = QScrollArea()
+        self.widget = QWidget()                 
+        self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setWidget(self.widget)
+        self.hbox.addWidget(self.scrollArea)
+        self.setGeometry(200, 200, 400, 100)
+        self.setWindowTitle('Spelling suggestions')
+        self.setLayout(self.hbox)
 
 # Call the main window class and maximize its view
 def main():
     app = QApplication(sys.argv)
     ex = MainWindow()
-    ex.showMaximized()
+    # ex.showMaximized()
     sys.exit(app.exec_())
 	
 if __name__ == '__main__':
